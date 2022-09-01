8f3d9756c7dca1d02bbeba28ce347589d3d689cd
DanielKloh <103609213+DanielKloh@users.noreply.github.com>
2022-09-01 19:23:50 -0300

    Add files via upload

---
+++ WF_Material/WF_Material/Form1.Designer.cs
@@ -0,0 +1,188 @@
+﻿namespace WF_Material
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtUsuario = new MaterialSkin.Controls.MaterialSingleLineTextField();
+            this.txtSenha = new MaterialSkin.Controls.MaterialSingleLineTextField();
+            this.btnLogin = new MaterialSkin.Controls.MaterialRaisedButton();
+            this.chkLembrarme = new MaterialSkin.Controls.MaterialCheckBox();
+            this.materialDivider1 = new MaterialSkin.Controls.MaterialDivider();
+            this.materialLabel1 = new MaterialSkin.Controls.MaterialLabel();
+            this.materialLabel2 = new MaterialSkin.Controls.MaterialLabel();
+            this.btnTema = new MaterialSkin.Controls.MaterialRaisedButton();
+            this.SuspendLayout();
+            // 
+            // txtUsuario
+            // 
+            this.txtUsuario.Depth = 0;
+            this.txtUsuario.Hint = "Usuário";
+            this.txtUsuario.Location = new System.Drawing.Point(92, 112);
+            this.txtUsuario.MouseState = MaterialSkin.MouseState.HOVER;
+            this.txtUsuario.Name = "txtUsuario";
+            this.txtUsuario.PasswordChar = '\0';
+            this.txtUsuario.SelectedText = "";
+            this.txtUsuario.SelectionLength = 0;
+            this.txtUsuario.SelectionStart = 0;
+            this.txtUsuario.Size = new System.Drawing.Size(325, 23);
+            this.txtUsuario.TabIndex = 0;
+            this.txtUsuario.UseSystemPasswordChar = false;
+            this.txtUsuario.TextAlignChanged += new System.EventHandler(this.txtUsuario_TextAlignChanged);
+            this.txtUsuario.TextChanged += new System.EventHandler(this.txtUsuario_TextChanged);
+            // 
+            // txtSenha
+            // 
+            this.txtSenha.Depth = 0;
+            this.txtSenha.Hint = "Senha";
+            this.txtSenha.Location = new System.Drawing.Point(92, 181);
+            this.txtSenha.MouseState = MaterialSkin.MouseState.HOVER;
+            this.txtSenha.Name = "txtSenha";
+            this.txtSenha.PasswordChar = '*';
+            this.txtSenha.SelectedText = "";
+            this.txtSenha.SelectionLength = 0;
+            this.txtSenha.SelectionStart = 0;
+            this.txtSenha.Size = new System.Drawing.Size(325, 23);
+            this.txtSenha.TabIndex = 1;
+            this.txtSenha.UseSystemPasswordChar = false;
+            this.txtSenha.TextChanged += new System.EventHandler(this.txtSenha_TextChanged);
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Depth = 0;
+            this.btnLogin.Enabled = false;
+            this.btnLogin.Location = new System.Drawing.Point(289, 258);
+            this.btnLogin.MouseState = MaterialSkin.MouseState.HOVER;
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Primary = true;
+            this.btnLogin.Size = new System.Drawing.Size(128, 32);
+            this.btnLogin.TabIndex = 2;
+            this.btnLogin.Text = "Login";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+            // 
+            // chkLembrarme
+            // 
+            this.chkLembrarme.AutoSize = true;
+            this.chkLembrarme.Depth = 0;
+            this.chkLembrarme.Enabled = false;
+            this.chkLembrarme.Font = new System.Drawing.Font("Roboto", 10F);
+            this.chkLembrarme.Location = new System.Drawing.Point(92, 258);
+            this.chkLembrarme.Margin = new System.Windows.Forms.Padding(0);
+            this.chkLembrarme.MouseLocation = new System.Drawing.Point(-1, -1);
+            this.chkLembrarme.MouseState = MaterialSkin.MouseState.HOVER;
+            this.chkLembrarme.Name = "chkLembrarme";
+            this.chkLembrarme.Ripple = true;
+            this.chkLembrarme.Size = new System.Drawing.Size(105, 30);
+            this.chkLembrarme.TabIndex = 3;
+            this.chkLembrarme.Text = "Lembrar-me";
+            this.chkLembrarme.UseVisualStyleBackColor = true;
+            // 
+            // materialDivider1
+            // 
+            this.materialDivider1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(31)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.materialDivider1.Depth = 0;
+            this.materialDivider1.Location = new System.Drawing.Point(52, 306);
+            this.materialDivider1.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialDivider1.Name = "materialDivider1";
+            this.materialDivider1.Size = new System.Drawing.Size(400, 2);
+            this.materialDivider1.TabIndex = 4;
+            this.materialDivider1.Text = "materialDivider1";
+            // 
+            // materialLabel1
+            // 
+            this.materialLabel1.AutoSize = true;
+            this.materialLabel1.Depth = 0;
+            this.materialLabel1.Font = new System.Drawing.Font("Roboto", 11F);
+            this.materialLabel1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.materialLabel1.Location = new System.Drawing.Point(66, 333);
+            this.materialLabel1.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialLabel1.Name = "materialLabel1";
+            this.materialLabel1.Size = new System.Drawing.Size(131, 19);
+            this.materialLabel1.TabIndex = 5;
+            this.materialLabel1.Text = "Esqueceu a senha";
+            // 
+            // materialLabel2
+            // 
+            this.materialLabel2.AutoSize = true;
+            this.materialLabel2.Depth = 0;
+            this.materialLabel2.Font = new System.Drawing.Font("Roboto", 11F);
+            this.materialLabel2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.materialLabel2.Location = new System.Drawing.Point(242, 333);
+            this.materialLabel2.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialLabel2.Name = "materialLabel2";
+            this.materialLabel2.Size = new System.Drawing.Size(69, 19);
+            this.materialLabel2.TabIndex = 6;
+            this.materialLabel2.Text = "Registrar";
+            // 
+            // btnTema
+            // 
+            this.btnTema.Depth = 0;
+            this.btnTema.Location = new System.Drawing.Point(351, 329);
+            this.btnTema.MouseState = MaterialSkin.MouseState.HOVER;
+            this.btnTema.Name = "btnTema";
+            this.btnTema.Primary = true;
+            this.btnTema.Size = new System.Drawing.Size(75, 23);
+            this.btnTema.TabIndex = 8;
+            this.btnTema.Text = "tema";
+            this.btnTema.UseVisualStyleBackColor = true;
+            this.btnTema.Click += new System.EventHandler(this.btnTema_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(525, 406);
+            this.Controls.Add(this.btnTema);
+            this.Controls.Add(this.materialLabel2);
+            this.Controls.Add(this.materialLabel1);
+            this.Controls.Add(this.materialDivider1);
+            this.Controls.Add(this.chkLembrarme);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.txtSenha);
+            this.Controls.Add(this.txtUsuario);
+            this.Name = "Form1";
+            this.Text = "Login";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private MaterialSkin.Controls.MaterialSingleLineTextField txtUsuario;
+        private MaterialSkin.Controls.MaterialSingleLineTextField txtSenha;
+        private MaterialSkin.Controls.MaterialRaisedButton btnLogin;
+        private MaterialSkin.Controls.MaterialCheckBox chkLembrarme;
+        private MaterialSkin.Controls.MaterialDivider materialDivider1;
+        private MaterialSkin.Controls.MaterialLabel materialLabel1;
+        private MaterialSkin.Controls.MaterialLabel materialLabel2;
+        private MaterialSkin.Controls.MaterialRaisedButton btnTema;
+    }
+}
+
---
+++ WF_Material/WF_Material/Form1.cs
@@ -0,0 +1,99 @@
+﻿using MaterialSkin;
+using MaterialSkin.Controls;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WF_Material
+{
+    public partial class Form1 : MaterialForm
+    {
+        MaterialSkinManager manager;
+        public Form1()
+        {
+            InitializeComponent();
+            //criar o gerenciador dos temas
+            //adidcionar no gerrenciador o Form
+            manager = MaterialSkinManager.Instance;
+            manager.AddFormToManage(this);
+
+            //Determinar o tema do form
+            manager.Theme = MaterialSkinManager.Themes.DARK;
+
+            //definiçao de cores
+
+            //Exemplos de temas
+            //manager.ColorScheme = new ColorScheme(Primary.Blue400, Primary.Blue500, Primary.Blue500,
+            //    Accent.LightBlue200, TextShade.WHITE);
+
+            manager.ColorScheme = new ColorScheme(Primary.Blue500, Primary.BlueGrey900, Primary.BlueGrey500,
+                Accent.Pink200, TextShade.WHITE);
+
+
+        }
+
+        private void txtUsuario_TextAlignChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void txtUsuario_TextChanged(object sender, EventArgs e)
+        {
+            //MessageBox.Show("TextChang");
+
+            if (txtUsuario.Text != "" && txtSenha.Text != "")
+            {
+                chkLembrarme.Enabled = true;
+                btnLogin.Enabled = true;
+            }
+            else {
+                chkLembrarme.Enabled = false;
+                btnLogin.Enabled = false;
+            }
+        }
+
+        private void txtSenha_TextChanged(object sender, EventArgs e) 
+        { 
+            //MessageBox.Show("TextChang");
+             if (txtUsuario.Text != "" && txtSenha.Text != "")
+            {
+                chkLembrarme.Enabled = true;
+                btnLogin.Enabled = true;
+            }
+            else
+            {
+                chkLembrarme.Enabled = false;
+                btnLogin.Enabled = false;
+            }
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Bem vindo " + txtUsuario.Text,"Login confirmado",MessageBoxButtons.OK,MessageBoxIcon.None);
+        }
+
+        private void materialFlatButton1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btnTema_Click(object sender, EventArgs e)
+        {
+            
+            if(manager.Theme == MaterialSkinManager.Themes.LIGHT)
+            {
+                manager.Theme = MaterialSkinManager.Themes.DARK;
+            }
+            else
+            {
+                manager.Theme = MaterialSkinManager.Themes.LIGHT;
+            }
+        }
+    }
+}
